04737a4b0946177d9b35f267d487126347079ab8
Durgesh-Patle <dpatle090096@gmail.com>
2025-01-23 15:21:23 +0530

    Enum in C#

---
+++ OOP's-C#/Enum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    enum weeks
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    public static void Main(string[] args)
+    {
+        weeks day = weeks.Monday;
+
+        Console.WriteLine("Day is: " + day);
+    }
+}
